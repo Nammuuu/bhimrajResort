7fa484a1fc0abb2d1280b53eb6c1c403120f95a1
Nammuuu <88241109+Nammuuu@users.noreply.github.com>
2024-04-03 10:36:07 -0400

    4 commit

--- lib/client.ts
+++ lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const client = createClient({
-  space: process.env.SPACE_ID || "",
-  accessToken: process.env.TOKEN || "",
+  space: process.env.SPACE_ID || "23iWbzLZLdPmb-tcA3fGJSKBRPrf4mMvbym2OZ0DRek",
+  accessToken: process.env.TOKEN || "e5zcpznyreto",
 });
 
 export default client
