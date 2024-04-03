de640103657b137ae61898bd153825b0da881a4a
Nammuuu <88241109+Nammuuu@users.noreply.github.com>
2024-04-03 10:06:53 -0400

    fisrt commit

---
+++ lib/components/Service.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const Service = ({ data }: { data: {png: StaticImageData,name : string} }) => {
+  return (
+    <div className="flex flex-col items-center space-y-2 m-3">
+      <Image src={data.png} alt="service"/>
+      <span className="font-light text-lg uppercase">
+          {data.name}
+      </span>
+    </div>
+  );
+};
+
+export default Service;
